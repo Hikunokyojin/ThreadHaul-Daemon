4557f3619e6def7aa2b8f8c7689585f0c22e552d
Divik Bhaskar <divikbhaskar@gmail.com>
2026-08-21 17:06:07 +0530

    Add missing ActuatorManager files

---
+++ Source/Actuator/ActuatorManager.cpp
@@ -0,0 +1,35 @@
+// ActuatorManager.cpp
+#include "ActuatorManager.h"
+
+namespace actuator {
+
+ActuatorManager::ActuatorManager(StateMachineConfig config,
+                                  TransitionCallback onTransition)
+    : config_(config)
+    , onTransition_(std::move(onTransition)) {}
+
+void ActuatorManager::AddBatchProcess(const std::wstring& processName, DWORD processId) {
+    if (machines_.find(processId) != machines_.end()) {
+        return; // already managed
+    }
+    machines_[processId] = std::make_unique<ProcessStateMachine>(
+        processName, processId, config_, onTransition_);
+}
+
+void ActuatorManager::RemoveBatchProcess(DWORD processId) {
+    machines_.erase(processId);
+}
+
+void ActuatorManager::UpdateAll(double criticalProcessCpuPercent) {
+    for (auto& [pid, machine] : machines_) {
+        machine->Update(criticalProcessCpuPercent);
+    }
+}
+
+void ActuatorManager::ForceRecoverAllForShutdown() {
+    for (auto& [pid, machine] : machines_) {
+        machine->ForceRecoverForShutdown();
+    }
+}
+
+} // namespace actuator
